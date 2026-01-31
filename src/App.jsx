2946d77cee4da0f98a62ea2c41338b7be845feda
Ruhi K <ruhik8789@gmail.com>
2026-01-31 23:26:29 +0530

    pushed the changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
 
 const App = () => {
+  const [sidebar, setSidebar] = useState(true);
   return (
     <div>
-    <h1>YouTube Clone Using React JS</h1>
+    <Navbar setSidebar={setSidebar}/>
+    <Routes>
+      <Route path='/' element={<Home sidebar={sidebar}/>}/>
+      <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
+    </Routes>
     </div>
   )
 }
